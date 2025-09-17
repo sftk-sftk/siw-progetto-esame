41650b6f4421b8aa06455fcea9a7e6280d5a956e
sftk-sftk <sftk@tutamail.com>
2025-09-17 17:14:24 +0200

    Project init

---
+++ README.md
@@ -0,0 +1,1 @@
+Siw Progetto Esame
---
+++ src/main/java/it/uniroma3/siw/SiwProgettoEsameApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwProgettoEsameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwProgettoEsameApplication.class, args);
+	}
+
+}
