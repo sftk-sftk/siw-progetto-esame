2491953f5b2c9d4f0029fc812e11ff63f0af6918
sftk-sftk <sftk@tutamail.com>
2025-09-25 10:38:04 +0200

    fetch Lazy da proodotto a tipologia
    fetch Lazy da proodotto a tipologia

--- src/main/java/it/uniroma3/siw/model/Prodotto.java
+++ src/main/java/it/uniroma3/siw/model/Prodotto.java
@@ -7,6 +7,7 @@ import java.util.Objects;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -48,7 +49,7 @@ public class Prodotto {
 	@OneToMany(mappedBy = "prodotto", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Commento> commenti = new ArrayList<>();
 
-	@ManyToOne(cascade = CascadeType.PERSIST)
+	@ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
 	private Tipologia tipologia;
 	
 	// Gestione lista commenti
